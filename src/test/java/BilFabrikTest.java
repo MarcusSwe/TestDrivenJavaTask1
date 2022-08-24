cbf3a3f9c93af728ebb200d8b96686df865dcab5
marcus.larsen <marcus.larsen.swe@gmail.com>
2022-08-24 19:27:14 +0200

    4 passed tests

--- src/test/java/BilFabrikTest.java
+++ src/test/java/BilFabrikTest.java
@@ -1,17 +1,33 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class BilFabrikTest {
 
     @Test
     public void getCarColorTest(){
-
         Car car = CarFactory.create("blue");
-
         assertEquals("blue", car.getColor());
+    }
 
+    @Test
+    public void getCarBrandTest(){
+        Car car = CarFactory.create("blue");
+        assertEquals("volvo", car.getBrand());
+    }
+
+    @Test
+    public void checkCarRegNrBelowSevenFigures(){
+        Car car = CarFactory.create("blue");
+        assertTrue(car.getRegnr() < 100000);
+    }
+
+    @Test
+    public void checkCarRegNrAboveZero(){
+        Car car = CarFactory.create("blue");
+        assertTrue(car.getRegnr() > -1);
     }
 
 }
